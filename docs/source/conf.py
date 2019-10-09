2dbebcd4d62dda38d154a16030f890bf9306a2b0
Adam Sierakowski <sierakowski@jhu.edu>
2019-10-09 15:47:04 -0400

    read the docs integration bugfix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,8 @@ project = 'craedl-sdk-python'
 copyright = '2019, The Johns Hopkins University'
 author = 'Craedl.org'
 
+master_doc = 'index'
+
 # The full version, including alpha/beta/rc tags
 release = __version__
 
